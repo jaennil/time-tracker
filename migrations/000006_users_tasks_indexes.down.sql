3e46a81d7da371a6cdf488ad8741cb8f36f952de
jaennil <jaennil@yandex.ru>
2024-07-09 21:21:40 +0300

    feat: task, user id indexes

---
+++ migrations/000006_users_tasks_indexes.down.sql
@@ -0,0 +1,2 @@
+DROP INDEX IF EXISTS user_id_idx;
+DROP INDEX IF EXISTS task_id_idx;
---
+++ migrations/000006_users_tasks_indexes.up.sql
@@ -0,0 +1,2 @@
+CREATE INDEX IF NOT EXISTS user_id_idx ON users(user_id);
+CREATE INDEX IF NOT EXISTS task_id_idx ON tasks(task_id);
